f947844f0366dd596e61bbbef4a6d3dd4e90f10b
Trent Oswald <trentoswald@therebelrobot.com>
2016-03-14 08:22:18 -0700

    trying out a new build script

--- build/countryjs.build.js
+++ build/countryjs.build.js
@@ -11,6 +11,8 @@ var _ = require('lodash')
 var str2snake = require('to-snake-case')
 var path = require('path')
 var fs = require('fs')
+var wikipedia = require("node-wikipedia");
+var wtf_wikipedia = require("wtf_wikipedia")
 
 request.get('https://raw.githubusercontent.com/mledoze/countries/master/dist/countries.json')
   .type('json')
@@ -20,9 +22,9 @@ request.get('https://raw.githubusercontent.com/mledoze/countries/master/dist/cou
     _readCountries(data)
   })
 
-function _readCountries (countries) {
-  _.forEach(countries, function (country) {
-    var name = country.name.common
+function _readCountries (liveCountries) {
+  _.forEach(liveCountries, function (liveCountry) {
+    var name = liveCountry.name.common
     var filename = str2snake(name)
     console.log(filename)
     try {
@@ -31,38 +33,77 @@ function _readCountries (countries) {
       var isFile = file.isFile()
       if (isFile) {
         console.log('File exists!', name, filename, fullPath)
-        var data = JSON.parse(fs.readFileSync(fullPath).toString())
-        _buildCountry(filename, country, data)
+        var oldCountry = JSON.parse(fs.readFileSync(fullPath).toString())
+        _buildCountry(filename, liveCountry, oldCountry)
       } else {
-        console.error('File doesnt exist!', name, filename, fullPath)
+        console.error('File is not file!', name, filename, fullPath)
+        _buildCountry(filename, liveCountry)
       }
     } catch(e) {
       console.error('File doesnt exist!', name, filename)
-      _buildCountry(filename, country)
+      _buildCountry(filename, liveCountry)
     }
   })
 }
 
-function _buildCountry (filename, newData, oldData) {
-  oldData = oldData || {}
-  // console.log(filename, newData, oldData)
-  _.forEach(newData, function (value, key) {
-    oldData[key] = value
+function _buildCountry (filename, liveCountry, legacyCountry) {
+  console.log('_buildCountry', filename, _.keys(liveCountry), _.keys(legacyCountry))
+  legacyCountry = legacyCountry || {}
+  _callWiki(liveCountry).then((wiki) => {
+    delete liveCountry.currencies
+    delete liveCountry.callingCodes
+    liveCountry.codes = liveCountry.codes || {}
+    liveCountry.codes.cca2 = liveCountry.cca2
+    liveCountry.codes.ccn3 = liveCountry.ccn3
+    liveCountry.codes.cca3 = liveCountry.cca3
+    liveCountry.codes.cioc = liveCountry.cioc
+    delete liveCountry.ISO
+    delete liveCountry.cca2
+    delete liveCountry.ccn3
+    delete liveCountry.cca3
+    delete liveCountry.cioc
+    liveCountry.provinces = legacyCountry.provinces
+    liveCountry.geoJSON = legacyCountry.geoJSON
+    liveCountry.flag = legacyCountry.flag
+    if(!legacyCountry.provinces || !legacyCountry.provinces.length){
+      // retrieve provinces from wikipedia
+    }
+    if(!liveCountry.flag || liveCountry.flag === ''){
+      if(wiki.image_flag && wiki.image_flag.text){
+        liveCountry.flag = 'https://en.m.wikipedia.org/wiki/File:'+wiki.image_flag.text
+      }
+    }
+    console.log(filename)
+    var stringDataToWrite = JSON.stringify(liveCountry, null, 2)
+    var fullPath = path.resolve(process.cwd(), './data/' + filename + '.json')
+    fs.writeFileSync(fullPath, stringDataToWrite)
   })
-  delete oldData.currencies
-  delete oldData.callingCodes
-  oldData.codes = oldData.codes || {}
-  oldData.codes.cca2 = oldData.cca2
-  oldData.codes.ccn3 = oldData.ccn3
-  oldData.codes.cca3 = oldData.cca3
-  oldData.codes.cioc = oldData.cioc
-  delete oldData.ISO
-  delete oldData.cca2
-  delete oldData.ccn3
-  delete oldData.cca3
-  delete oldData.cioc
-  console.log(filename, oldData)
-  var stringDataToWrite = JSON.stringify(oldData, null, 2)
-  var fullPath = path.resolve(process.cwd(), './data/' + filename + '.json')
-  fs.writeFileSync(fullPath, stringDataToWrite)
+  // console.log(filename, legacyCountry, liveCountry)
+
+}
+
+function _callWiki(country){
+  return new Promise((resolve, reject) =>{
+    wtf_wikipedia.from_api(country.name.common, "en", function(markup){
+      var wiki= wtf_wikipedia.parse(markup)
+      if(wiki.type === 'disambiguation'){
+        console.log(country.name.common, 'disambiguation. fetching flag from +(country)')
+        return wtf_wikipedia.from_api(country.name.common + ' (country)', "en", function(markup){
+          var wiki= wtf_wikipedia.parse(markup)
+          console.log(country.name.common, '(country) wiki', wiki)
+          console.log(country.name.common, '(country) infobox', wiki.infobox)
+          if(wiki.infobox && wiki.infobox.image_flag && wiki.infobox.image_flag.text){
+            console.log(country.name.common, '(country) flag', wiki.infobox.image_flag.text)
+          }
+          resolve(wiki.infobox)
+        })
+      }
+      console.log(country.name.common, 'infobox', wiki.infobox)
+      if(wiki.infobox && wiki.infobox.image_flag && wiki.infobox.image_flag.text){
+        console.log(country.name.common, 'flag', wiki.infobox.image_flag.text)
+        country.flag = 'https://en.m.wikipedia.org/wiki/File:'+wiki.infobox.image_flag.text
+      }
+      resolve(wiki.infobox)
+    })
+  })
 }
--- test/countryjs.test.js
+++ test/countryjs.test.js
@@ -1,239 +1,305 @@
-var expect = require('chai').expect
-var country = require('../index')
+'use strict'
 
-/* Definitions for JS Standard */
-/* global describe, it */
+const test = require('tape')
+const _ = require('lodash')
+const fs = require('fs')
+const path = require('path')
 
-describe('countryjs', function () {
-  it('should get all country information', function (done) {
-    var tester = country.all()
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(251)
-    expect(tester[0]).to.be.an('object')
-    done()
-  })
-  it('should get all available info for United States (default)', function (done) {
-    var tester = country.info('US')[0]
-    expect(tester).to.be.an('object')
-    done()
-  })
-  it('should get all available info for United States (ISO2)', function (done) {
-    var tester = country.info('US', 'ISO2')[0]
-    expect(tester).to.be.an('object')
-    done()
-  })
-  it('should get all available info for United States (ISO3)', function (done) {
-    var tester = country.info('USA', 'ISO3')[0]
-    expect(tester).to.be.an('object')
-    done()
-  })
-  it('should get all available info for United States (name)', function (done) {
-    var tester = country.info('United States', 'name')[0]
-    expect(tester).to.be.an('object')
-    done()
-  })
-  it('should get all available info for United States (alternate name)', function (done) {
-    var tester = country.info('United States of America', 'name')[0]
-    expect(tester).to.be.an('object')
-    done()
-  })
-  it('should get all available info for United States (translation name)', function (done) {
-    var tester = country.info('Estados Unidos', 'name')[0]
-    expect(tester).to.be.an('object')
-    done()
-  })
-  it('should get all available info using approximate string matching', function (done) {
-    var searches = {
-      'thailande': 'Thailand',
-      'U.S.A': 'United States',
-      'THE GREAT BRITAIN': 'United Kingdom',
-      ' Bosnia herzegovina ': 'Bosnia and Herzegovina'
-    }
-    Object.keys(searches).forEach(function (search) {
-      var tester = country.info(search, 'name')
-      expect(tester).to.be.an('object')
-      expect(tester.name).to.equal(searches[search])
-    })
-    done()
-  })
-  it('should get list of states for United States', function (done) {
-    var tester = country.states('US')[0].states
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(51)
-    expect(tester[0]).to.be.a('string')
-    done()
-  })
-  it('should get list of provinces for United States', function (done) {
-    var tester = country.provinces('US')[0].provinces
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(51)
-    expect(tester[0]).to.be.a('string')
-    done()
-  })
-  it('should get name for United States', function (done) {
-    var tester = country.name('US')[0].name
-    expect(tester).to.be.a('string')
-    done()
-  })
-  it('should get alternate spellings for United States', function (done) {
-    var tester = country.altSpellings('US')[0].altSpellings
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(3)
-    expect(tester[0]).to.be.a('string')
-    done()
-  })
-  it('should get area (km²) of United States', function (done) {
-    var tester = country.area('US')[0].area
-    expect(tester).to.be.a('number')
-    done()
-  })
-  it('should get list of bordering countries for United States', function (done) {
-    var tester = country.borders('US')[0].borders
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(2)
-    expect(tester[0]).to.be.a('string')
-    done()
-  })
-  it('should get list of calling codes for United States', function (done) {
-    var tester = country.callingCodes('US')[0].callingCodes
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(1)
-    expect(tester[0]).to.be.a('string')
-    done()
-  })
-  it('should get capital for United States', function (done) {
-    var tester = country.capital('US')[0].capital
-    expect(tester).to.be.a('string')
-    done()
-  })
-  it('should get list of currencies for United States', function (done) {
-    var tester = country.currencies('US')[0].currencies
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(3)
-    expect(tester[0]).to.be.a('string')
-    done()
-  })
-  it('should get demonym for United States', function (done) {
-    var tester = country.demonym('US')[0].demonym
-    expect(tester).to.be.a('string')
-    done()
-  })
-  it('should get flag for United States', function (done) {
-    var tester = country.flag('US')[0].flag
-    expect(tester).to.be.a('string')
-    done()
-  })
-  it('should get geoJSON for United States', function (done) {
-    var tester = country.geoJSON('US')[0].geoJSON
-    expect(tester).to.be.an('object')
-    done()
-  })
-  it('should get list of ISO codes for United States', function (done) {
-    var tester = country.ISOcodes('US')[0].ISO
-    expect(tester).to.be.an('object')
-    done()
-  })
-  it('should get list of languages for United States', function (done) {
-    var tester = country.languages('US')[0].languages
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(1)
-    expect(tester[0]).to.be.a('string')
-    done()
-  })
-  it('should get approximate latitude and longitude coordinates for United States', function (done) {
-    var tester = country.latlng('US')[0].latlng
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(2)
-    expect(tester[0]).to.be.a('number')
-    done()
-  })
-  it('should get native name for United States', function (done) {
-    var tester = country.nativeName('US')[0].nativeName
-    expect(tester).to.be.a('string')
-    done()
-  })
-  it('should get population for United States', function (done) {
-    var tester = country.population('US')[0].population
-    expect(tester).to.be.a('number')
-    done()
-  })
-  it('should get region for United States', function (done) {
-    var tester = country.region('US')[0].region
-    expect(tester).to.be.a('string')
-    done()
-  })
-  it('should get subregion for United States', function (done) {
-    var tester = country.subregion('US')[0].subregion
-    expect(tester).to.be.a('string')
-    done()
-  })
-  it('should get list of timezones for United States', function (done) {
-    var tester = country.timezones('US')[0].timezones
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(11)
-    expect(tester[0]).to.be.a('string')
-    done()
-  })
-  it('should get tld for United States', function (done) {
-    var tester = country.tld('US')[0].tld
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(1)
-    expect(tester[0]).to.be.a('string')
-    done()
-  })
-  it('should get list of translations of the name of United States', function (done) {
-    var tester = country.translations('US')[0].translations
-    expect(tester).to.be.an('object')
-    done()
-  })
-  it('should get wiki page link for United States', function (done) {
-    var tester = country.wiki('US')[0].wiki
-    expect(tester).to.be.a('string')
-    done()
-  })
-  it('should get info for all countries under same ISO code', function (done) {
-    var tester = country.info('GB')
-    expect(tester).to.be.an('array')
-    expect(tester[0].name).to.equal('Scotland')
-    expect(tester[1].name).to.equal('United Kingdom')
-    done()
-  })
-  it('should undefined for a mismatched country identifier', function (done) {
-    var tester = country.info('UX')
-    expect(tester).to.be.an('array')
-    expect(tester.length).to.equal(0)
-    done()
-  })
-  it('should undefined for a mismatched country identifier (other methods)', function (done) {
-    var methods = [
-      'states',
-      'provinces',
-      'name',
-      'altSpellings',
-      'area',
-      'borders',
-      'callingCodes',
-      'capital',
-      'currencies',
-      'demonym',
-      'flag',
-      'geoJSON',
-      'ISOcodes',
-      'languages',
-      'latlng',
-      'nativeName',
-      'population',
-      'region',
-      'subregion',
-      'timezones',
-      'tld',
-      'translations',
-      'wiki'
-    ]
-    methods.forEach(function (method) {
-      var tester = country[method]('UX')
-      expect(tester).to.be.an('undefined')
-    })
-    done()
-  })
+const country = require('../lib/countryjs')
+const dataDir = fs.readdirSync(path.resolve(__dirname, '../data'))
+const staticUS = require('../data/united_states.json')
+
+process.env.DEBUG = 'function,block'
+
+test('it should get all country information', (t) => {
+  t.plan(2)
+  let testCountry = country.all()
+  t.ok(_.isArray(testCountry), 'should be array')
+  t.equal(testCountry.length, dataDir.length)
+  t.end() // t.end(err)
 })
+
+test('it should get all available info for United States (default)', (t) => {
+  t.plan(2)
+  let testCountry = country.info('US')
+  t.ok(_.isArray(testCountry), 'should be array')
+  t.deepEqual(testCountry[0], staticUS)
+  t.end() // t.end(err)
+})
+
+test('it should get all available info for United States (ISO2/CCA2)', (t) => {
+  t.plan(2)
+  let testCountry = country.info('US', 'cca2')
+  t.ok(_.isArray(testCountry), 'should be array')
+  t.deepEqual(testCountry[0], staticUS)
+  t.end() // t.end(err)
+})
+
+test('it should get all available info for United States (ISO3/CCA3)', (t) => {
+  t.plan(2)
+  let testCountry = country.info('USA', 'cca3')
+  t.ok(_.isArray(testCountry), 'should be array')
+  t.deepEqual(testCountry[0], staticUS)
+  t.end() // t.end(err)
+})
+
+test('it should get all available info for United States (CCN3)', (t) => {
+  t.plan(2)
+  let testCountry = country.info('840', 'ccn3')
+  t.ok(_.isArray(testCountry), 'should be array')
+  t.deepEqual(testCountry[0], staticUS)
+  t.end() // t.end(err)
+})
+
+test('it should get all available info for United States (CIOC)', (t) => {
+  t.plan(2)
+  let testCountry = country.info('USA', 'cioc')
+  t.ok(_.isArray(testCountry), 'should be array')
+  t.deepEqual(testCountry[0], staticUS)
+  t.end() // t.end(err)
+})
+
+test('it should get all available info for United States (name - common)', (t) => {
+  t.plan(2)
+  let testCountry = country.info('United States', 'name')
+  t.ok(_.isArray(testCountry), 'should be array')
+  t.deepEqual(testCountry[0], staticUS)
+  t.end() // t.end(err)
+})
+
+test('it should get all available info for United States (name - official)', (t) => {
+  t.plan(2)
+  let testCountry = country.info('United States of America', 'name')
+  t.ok(_.isArray(testCountry), 'should be array')
+  t.deepEqual(testCountry[0], staticUS)
+  t.end() // t.end(err)
+})
+
+test('it should get all available info for United States (name - translation - common)', (t) => {
+  t.plan(2)
+  let testCountry = country.info('アメリカ合衆国', 'name')
+  t.ok(_.isArray(testCountry), 'should be array')
+  t.deepEqual(testCountry[0], staticUS)
+  t.end() // t.end(err)
+})
+
+test('it should get all available info for United States (name - translation - official)', (t) => {
+  t.plan(2)
+  let testCountry = country.info("Les états-unis d'Amérique", 'name')
+  t.ok(_.isArray(testCountry), 'should be array')
+  t.deepEqual(testCountry[0], staticUS)
+  t.end() // t.end(err)
+})
+
+test('it should get all available info using approximate string matching', (t) => {
+  t.plan(12)
+  var searches = {
+    'thailande': 'Thailand',
+    'U.S.A': 'United States',
+    'THE GREAT BRITAIN': 'United Kingdom',
+    ' Bosnia herzegovina ': 'Bosnia and Herzegovina'
+  }
+  Object.keys(searches).forEach(function (search) {
+    let testCountry = country.info(search, 'name')
+    t.ok(_.isArray(testCountry), 'should be array')
+    t.ok(_.isObject(testCountry[0]), 'should be object')
+    t.equal(testCountry[0].name.common, searches[search])
+  })
+  t.end() // t.end(err)
+})
+//
+// test('it should get list of states for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get list of provinces for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get name for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get alternate spellings for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get area (km²) of United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get list of bordering countries for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get list of calling codes for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get capital for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get list of currencies for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get demonym for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get flag for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get geoJSON for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get list of ISO codes for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get list of languages for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get approximate latitude and longitude coordinates for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get native name for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get population for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get region for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get subregion for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get list of timezones for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get tld for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get list of translations of the name of United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get wiki page link for United States', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get info for all countries under same ISO code', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should undefined for a mismatched country identifier', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should get info for all countries under same ISO code', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should undefined for a mismatched country identifier', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
+//
+// test('it should undefined for a mismatched country identifier (other methods)', (t) => {
+//   t.plan(0)
+//   let testCountry = country
+//   t.equal()
+//   t.end() // t.end(err)
+// })
